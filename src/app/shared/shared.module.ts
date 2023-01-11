5244245eeac15bcceedd48d4f482281075b12be0
Dejan Jankovic <deki1984@hotmail.de>
2023-01-11 20:22:24 +0100

    create topbar component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TopbarComponent } from './topbar/topbar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TopbarComponent
+  ],
   imports: [
     CommonModule
   ]
