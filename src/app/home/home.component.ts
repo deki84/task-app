f80feb72c0c19228c4eb9ef8f6e36fa4f5d4af58
Dejan Jankovic <deki1984@hotmail.de>
2023-01-12 15:51:13 +0100

    style home and about component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,20 @@
 import { DataService } from './../shared/services/data.service';
 import { Component, OnInit } from '@angular/core';
-
+import { Task } from '../shared/models/task.model';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  public tasks: Task[] = [];
+
   constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
     this.dataService.getTasks().subscribe(
-      (response: any) => {
-        console.log('Data', response);
+      (response: Task[]) => {
+        this.tasks = response;
       },
       (error) => {
         console.log(error);
--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Task } from '../models/task.model';
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
   constructor(private http: HttpClient) {}
 
-  public getTasks(): Observable<any> {
-    return this.http.get('https://jsonplaceholder.typicode.com/todos?_limit=5');
+  public getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(
+      'https://jsonplaceholder.typicode.com/todos?_limit=5'
+    );
   }
 }
